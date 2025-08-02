0e43154945c25a1261b50e4382822e33748f42af
Ranjeeeth <2420030569@klh.edu.in>
2025-08-02 09:41:31 +0530

    Add files via upload

---
+++ operators/LeftshitRightshif.java
@@ -0,0 +1,12 @@
+package operators;
+import java.util.Scanner;
+public class LeftshitRightshif {
+	public static void main(String[] args) {
+	    // TODO Auto-generated method stub
+	    System.out.println("Enter a numbers: ");
+	        Scanner sc = new Scanner(System.in);
+	        int a=sc.nextInt();
+	    System.out.println("Right shift by 2: "+(a>>2));
+	    System.out.println("Left shift by 2: "+(2<<a));
+	  }
+}
---
+++ operators/StudentClass.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class StudentClass {
+	public static void main(String[] args) {
+//	 Check if a student passed both theory and practical exams using logical AND.
+	boolean theory= false;
+	boolean practical=true;
+	if(theory&&practical) {
+		System.out.println("Student passed in both");
+	}else {
+		System.out.println("Student didn,t pass in both : so he fialed");
+	}
+}
+	}
